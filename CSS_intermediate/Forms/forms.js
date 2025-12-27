f4dd1d532414cf4442139940d32d7a6edc35b70c
CreatineCoder <devanshagrawal1027@gmail.come>
2025-12-27 12:44:55 +0530

    froms learned

---
+++ CSS_intermediate/Forms/forms.js
@@ -0,0 +1,5 @@
+const nameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const password = document.getElementById('password');
+const age = document.getElementById('age');
+const bio = document.getElementById('bio');
